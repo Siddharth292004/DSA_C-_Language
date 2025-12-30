636d304e1d7a843d43a7cb4b4e810caa014f1c88
Siddharth292004 <sidharth9509@gmail.com>
2025-12-30 10:44:38 +0530

    initial commit

--- Array/function_array/insert_pos.cpp
+++ Array/function_array/insert_pos.cpp
@@ -1,39 +1,51 @@
 #include <iostream>
 using namespace std;
 
-void insertAtPosition(int arr[], int &size, int pos, int newElement) {
-    // shift elements to right from the position
-    for(int i = size; i > pos; i--) {
+void insertAtPosition(int arr[], int &size, int pos, int newElement)
+{
+    // shift elements to right
+    for(int i = size; i >= pos; i--)
+    {
         arr[i] = arr[i - 1];
     }
 
-    // insert new element at the specified position
-    arr[pos] = newElement;
+    // insert element
+    arr[pos - 1] = newElement;
     size++;
-
-    cout << "New array: ";
-    for(int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
 }
 
-int main(){
+int main()
+{
     int arr[100], size, pos, newElement;
+
     cout << "Enter the size of the array: ";
     cin >> size;
-    cout << "Enter the " << size << " elements in the array: ";
-    for(int i = 0; i < size; i++){
+
+    cout << "Enter the " << size << " elements: ";
+    for(int i = 0; i < size; i++)
+    {
         cin >> arr[i];
     }
+
     cout << "Current array: ";
-    for(int i = 0; i < size; i++){
+    for(int i = 0; i < size; i++)
+    {
         cout << arr[i] << " ";
     }
-    cout << "\nEnter the position to insert the new element (0 to " << size << "): ";
+
+    cout << "\nEnter the position (1 to " << size + 1 << "): ";
     cin >> pos;
+
     cout << "Enter the element to insert: ";
     cin >> newElement;
 
-    // Shift elements to the right from the position
+    insertAtPosition(arr, size, pos, newElement);
 
+    cout << "New array: ";
+    for(int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
 }
